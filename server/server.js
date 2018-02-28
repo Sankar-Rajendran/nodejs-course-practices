5d944d3730d70b6cfec57461cfc305f3ff911e74
sankar.rajendran <sankar.rajendran@imaginea.com>
2018-02-28 15:32:09 +0530

    validation added

--- server/server.js
+++ server/server.js
@@ -6,13 +6,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todos', {
     text: {
-        type: String
+        type: String,
+        required:true,
+        minlength:1,
+        trim:true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default:false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default:null
     }
 });
 
@@ -29,12 +34,11 @@ newTodo.save().then((result) => {
 })
 
 
-
 var newTodo = new Todo({
     error:false
 });
 
-
+//Throws error - since required field not there in object
 newTodo.save().then((result) => {
     console.log('Todos', result);
 }, (e) => {
