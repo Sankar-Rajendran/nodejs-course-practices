f668bdc72e09f2bbfcef7e93d9fbddaf20100677
sankar.rajendran <sankar.rajendran@imaginea.com>
2018-03-14 16:31:46 +0530

    User login and token creation

--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ const config = require('./config/config.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
-var {authenticate} = require('./middleware/authenticate')
+var { authenticate } = require('./middleware/authenticate')
 
 
 
@@ -113,7 +114,7 @@ app.patch('/todos/:id', (req, res) => {
 
 //User Routes
 app.post('/users', (req, res) => {
-    var userProps = _.pick(req.body, ['email', 'password'])
+    var userProps = _.pick(req.body, ['email', 'password']);
     var user = new User(userProps);
     user.save().then(() => {
         return user.generateAuthToken();
@@ -141,6 +142,18 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 })
 
+
+app.post('/users/login', (req, res) => {
+    var userProps = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(userProps.email, userProps.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((err) => {
+        res.status(400).send({})
+    })
+
+})
 
 
 
