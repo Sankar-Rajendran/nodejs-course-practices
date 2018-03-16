6cbdd21a4c5cca0381207e038e006491ecd23e87
sankar.rajendran <sankar.rajendran@imaginea.com>
2018-03-16 12:35:26 +0530

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -65,7 +65,7 @@ userSchema.methods.generateAuthToken = function () {
     //called with document
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET).toString();
     user.tokens.push({ access: access, token });
     return user.save().then(() => {
         return token;
@@ -92,7 +92,7 @@ userSchema.statics.findByToken = function (token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123')
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
     } catch (err) {
         // return new Promise((resolve, reject) => {
         //     reject();
